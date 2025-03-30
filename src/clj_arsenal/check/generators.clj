e2809fed9bdd31b5169cdc7ee82921e990574466
Ray Stubbs <stubbs.ray@gmail.com>
2025-03-29 21:15:03 -0600

    make generators use the provided seed

--- src/clj_arsenal/check/generators.clj
+++ src/clj_arsenal/check/generators.clj
@@ -1,24 +1,30 @@
 (ns ^:no-doc clj-arsenal.check.generators
   (:require
-   [clojure.string :as str]))
+   [clojure.string :as str])
+  (:import
+   java.util.Random))
 
-(defn- rand-int'
-  [min max]
-  (+ min (rand-int (- (inc max) min))))
+(defn- rng-next-int
+  [^Random rng min max]
+  (+ min (rem (.nextLong rng) (- (inc max) min))))
 
 (defn integer
-  [& {:keys [max min limit] :or {min 0 max 2147483647 limit 32}}]
-  (take limit
-    (cond-> []
-      (some? min) (conj min)
-      (some? max) (conj max)
-      true (into (repeatedly limit #(rand-int' min max))))))
+  [& {:keys [max min limit seed] :or {min 0 max 2147483647 limit 32 seed 0}}]
+  (let
+    [rng (Random. seed)]
+    (take limit
+      (cond-> []
+        (some? min) (conj min)
+        (some? max) (conj max)
+        true (into (repeatedly limit #(rng-next-int rng min max)))))))
 
 (defn string
-  [& {:keys [min-len max-len limit] :or {min-len 0 max-len 32 limit 32}}]
-  (repeatedly limit
-    (fn []
-      (str/join
-        (repeatedly
-          (rand-int' min-len max-len)
-          #(char (rand-int' 32 127)))))))
+  [& {:keys [min-len max-len limit seed] :or {min-len 3 max-len 32 limit 32 seed 0}}]
+  (let
+    [rng (Random. seed)]
+    (repeatedly limit
+      (fn []
+        (str/join
+          (repeatedly
+            (rng-next-int rng min-len max-len)
+            #(char (rng-next-int rng 32 127))))))))
